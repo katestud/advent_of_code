01c5d5462830a8ae8cc098cdcb91093fd682c609
Kate Studwell <katestudwell@gmail.com>
2017-12-07 22:19:51 -0500

    Solve part 2 of day 4

--- day_04/passphrase_test.exs
+++ day_04/passphrase_test.exs
@@ -11,4 +11,11 @@ defmodule PassphraseTest do
     assert PassphraseOne.count_valid("passphrase_inputs.txt") == 337
   end
 
+  test "part two finds count of valid phrases that are not anagrams" do
+    assert PassphraseTwo.count_valid("passphrase_anagram_test_inputs.txt") == 3
+    assert PassphraseTwo.valid_passphrase?("iiii oiii ooii oooi oooo")
+    refute PassphraseTwo.valid_passphrase?("oiii ioii iioi iiio")
+    assert PassphraseTwo.count_valid("passphrase_inputs.txt") == 231
+  end
+
 end
