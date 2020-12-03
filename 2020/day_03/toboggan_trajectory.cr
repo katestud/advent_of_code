c36e24b1c63485309754d4c6ee9c83b53b2aff33
Kate Studwell <katestud@github.com>
2020-12-03 08:34:58 -0500

    Add part 2 solution

--- 2020/day_03/toboggan_trajectory.cr
+++ 2020/day_03/toboggan_trajectory.cr
@@ -6,16 +6,31 @@ class Trajectory
     @hill_grid = File.read_lines(input_file)
   end
 
-  def traverse
-    index = 0
+  def traverse(right = 3, skip_rows = false)
+    position = 0
     tree_count = 0
-    @hill_grid.each do |row|
-      adjusted_index = index % row.size
-      tree_count += 1 if row[adjusted_index] == '#'
-      index += 3
+    @hill_grid.each_with_index do |row, row_index|
+      next if skip_rows && row_index.odd?
+      adjusted_position = position % row.size
+      tree_count += 1 if row[adjusted_position] == '#'
+      position += right
     end
     tree_count
+  end
+
+  # Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+  # Right 1, down 1.
+  # Right 3, down 1. (This is the slope you already checked.)
+  # Right 5, down 1.
+  # Right 7, down 1.
+  # Right 1, down 2.
+
+  def optimize
+    down1_product = [1, 3, 5, 7].reduce(1) { |acc, i| acc * traverse(i) }
+    down1_product * traverse(1, true)
   end
 end
 
 puts Trajectory.new.traverse
+puts Trajectory.new.optimize
--- 2020/day_03/toboggan_trajectory_test.cr
+++ 2020/day_03/toboggan_trajectory_test.cr
@@ -4,8 +4,14 @@ require "./toboggan_trajectory"
 describe Trajectory do
 
   describe "#finds_tree_count" do
-    it "finds all the threes by moving to the right three and down one" do
+    it "finds all the trees by moving to the right three and down one" do
       Trajectory.new("test_input.txt").traverse.should eq 7
+    end
+  end
+
+  describe "#compares all routes" do
+    it "compares multiple routes and returns the product of the trees" do
+      Trajectory.new("test_input.txt").optimize.should eq 336
     end
   end
 end
