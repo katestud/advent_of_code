c6453c26227cb94e3300d995452b39cbff8166ea
Kate Studwell <katestud@github.com>
2020-12-04 13:07:30 -0500

    Day 4 part 2

--- 2020/day_04/passport_processing.exs
+++ 2020/day_04/passport_processing.exs
@@ -24,15 +24,73 @@
 # Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
 
 defmodule PassportSystem do
-  def count_valid(file_path) do
-    File.read!(file_path)
-    |> String.split("\n\n")
-    |> Enum.map(&mapify/1)
+
+  @eye_color ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+  @hair_color_format ~r/[0-9a-f]/
+
+  def count_valid_keys(file_path) do
+    scrubbed_input(file_path)
     |> Enum.count(&valid?/1)
   end
 
-  defp valid?(%{"byr" => birth_year, "iyr" => issue_year, "eyr" => exp_year, "hgt" => height, "hcl" => hair_color, "ecl" => eye_color, "pid" => passport_id}), do: true
+  def count_valid_format(file_path) do
+    scrubbed_input(file_path)
+    |> Enum.count(&valid_format?/1)
+  end
+
+  defp valid?(%{"byr" => _byr, "iyr" => _iyr, "eyr" => _eyr, "hgt" => _hgt, "hcl" => _hcl, "ecl" => _ecl, "pid" => _pid}), do: true
   defp valid?(_), do: false
+
+  defp valid_format?(%{"byr" => birth_year, "iyr" => issue_year, "eyr" => exp_year, "hgt" => height, "hcl" => hair_color, "ecl" => eye_color, "pid" => passport_id}) do
+    valid_birth_year?(String.to_integer(birth_year)) &&
+    valid_issue_year?(String.to_integer(issue_year)) &&
+    valid_expiration_year?(String.to_integer(exp_year)) &&
+    valid_height?(height) &&
+    valid_hair_color?(hair_color) &&
+    valid_eye_color?(eye_color) &&
+    valid_passport_id?(passport_id)
+  end
+  defp valid_format?(_), do: false
+
+  defp valid_year?(year), do: 4 == Integer.digits(year) |> Enum.count
+
+  # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  defp valid_birth_year?(year), do: valid_year?(year) && Enum.member?(1920..2002, year)
+
+  # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  defp valid_issue_year?(year), do: valid_year?(year) && Enum.member?(2010..2020, year)
+
+  # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  defp valid_expiration_year?(year), do: valid_year?(year) && Enum.member?(2020..2030, year)
+
+  # hgt (Height) - a number followed by either cm or in:
+  # If cm, the number must be at least 150 and at most 193.
+  # If in, the number must be at least 59 and at most 76.
+  defp valid_height?(height) do
+     cond do
+      String.ends_with?(height, "cm") ->
+        num = String.trim(height, "cm") |> String.to_integer
+        Enum.member?(150..193, num)
+      String.ends_with?(height, "in") ->
+        num = String.trim(height, "in") |> String.to_integer
+        Enum.member?(59..76, num)
+      true ->
+        false
+    end
+  end
+
+  # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  defp valid_hair_color?(color) do
+    String.starts_with?(color, "#") &&
+    Regex.match?(@hair_color_format, color) &&
+    String.length(color) == 7
+  end
+
+  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  defp valid_eye_color?(color), do: Enum.member?(@eye_color, color)
+
+  # pid (Passport ID) - a nine-digit number, including leading zeroes.
+  defp valid_passport_id?(id), do: 9 == String.length(id) && Regex.match?(~r/[0-9]/, id)
 
   defp mapify(passport_data) when is_list(passport_data) do
     Enum.reduce(passport_data, [], fn(line, result) -> result ++ String.split(line, ":") end)
@@ -43,6 +101,12 @@ defmodule PassportSystem do
     passport_data
     |> String.split([" ", "\n"])
     |> mapify
+  end
+
+  defp scrubbed_input(file_path) do
+    File.read!(file_path)
+    |> String.split("\n\n")
+    |> Enum.map(&mapify/1)
   end
 
 end
--- 2020/day_04/passport_processing_test.exs
+++ 2020/day_04/passport_processing_test.exs
@@ -1,4 +1,4 @@
-Code.load_file("passport_processing.exs", __DIR__)
+Code.require_file("passport_processing.exs", __DIR__)
 
 ExUnit.start
 
@@ -6,9 +6,15 @@ defmodule PassportProcessingTest do
   use ExUnit.Case
 
   test "finds valid passport count for part 1" do
-    assert 2 == PassportSystem.count_valid("test_input.txt")
+    assert 2 == PassportSystem.count_valid_keys("test_input.txt")
 
-    assert 230 == PassportSystem.count_valid("input.txt")
+    assert 230 == PassportSystem.count_valid_keys("input.txt")
+  end
+
+  test "finds valid passport format count for part 2" do
+    assert 4 == PassportSystem.count_valid_format("part2_test_input.txt")
+
+    assert 156 == PassportSystem.count_valid_format("input.txt")
   end
 
 end
