7d3184fb866e9a33d45feb3f8c3c023a4dd9fa8b
Kate Studwell <katestud@github.com>
2020-12-05 05:21:33 -0500

    Day 5 part 1

---
+++ 2020/day_05/binary_boarding.exs
@@ -0,0 +1,66 @@
+defmodule BinaryBoarding do
+
+#   Enum.chunk_every([1, 2, 3, 4, 5], 2, 3, [])
+# [[1, 2], [4, 5]]
+
+  @row_range Enum.to_list(0..127)
+  @column_range Enum.to_list(0..7)
+
+  def max_id(filename) do
+    input(filename)
+    |> Enum.map(&seat_id/1)
+    |> Enum.max
+  end
+
+  defp seat_id(<<row::bytes-size(7)>> <> <<column::bytes-size(3)>>) do
+    row(row) * 8 + column(column)
+  end
+  defp seat_id(_), do: 0
+
+  defp row(row) do
+    row(String.codepoints(row), @row_range)
+  end
+
+  defp row([], [range_head | _]) do
+    range_head
+  end
+  defp row([row_head | row_tail], range) do
+    new_range = row(row_head, range)
+    row(row_tail, new_range)
+  end
+
+  defp row("B", range) do
+    [_lower, upper] = Enum.chunk_every(range, trunc(length(range) / 2))
+    upper
+  end
+  defp row("F", range) do
+    [lower | _upper] = Enum.chunk_every(range, trunc(length(range) / 2))
+    lower
+  end
+
+  defp column(column) do
+    column(String.codepoints(column), @column_range)
+  end
+
+  defp column([], [range_head | _]) do
+    range_head
+  end
+  defp column([column_head | column_tail], range) do
+    new_range = column(column_head, range)
+    column(column_tail, new_range)
+  end
+
+  defp column("R", range) do
+    [_lower, upper] = Enum.chunk_every(range, trunc(length(range) / 2))
+    upper
+  end
+  defp column("L", range) do
+    [lower | _upper] = Enum.chunk_every(range, trunc(length(range) / 2))
+    lower
+  end
+
+  defp input(filename) do
+    File.read!(filename)
+    |> String.split("\n")
+  end
+end
---
+++ 2020/day_05/binary_boarding_test.exs
@@ -0,0 +1,14 @@
+Code.require_file("binary_boarding.exs", __DIR__)
+
+ExUnit.start
+
+defmodule BinaryBoardingTest do
+  use ExUnit.Case
+
+  test "finds highest seat id" do
+    assert 820 == BinaryBoarding.max_id("test_input.txt")
+
+    assert 904 == BinaryBoarding.max_id("input.txt")
+  end
+
+end
