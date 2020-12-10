8fe60fb711aaec4b9b803ece5d75d555bb02ba06
Kate Studwell <katestud@github.com>
2020-12-10 17:31:37 -0500

    Part 2

--- 2020/day_10/adapter_arrays.rb
+++ 2020/day_10/adapter_arrays.rb
@@ -1,13 +1,26 @@
-input = File.readlines("input.txt").map(&:to_i)
+input = File.readlines("input.txt").map(&:to_i).sort
 
-input << 0 # to account for starting output
+input.unshift(0) # to account for starting output
 
 diffs = Hash.new(0)
-input.sort.each_cons(2) do |adapter, next_adapter|
+input.each_cons(2) do |adapter, next_adapter|
   diff = next_adapter - adapter
   diffs[next_adapter - adapter] += 1
 end
 
 diffs[3] += 1 # to account for your device
 
+puts "Product of Ones and Threes:"
 puts diffs[1] * diffs[3]
+
+# Loosely inspired by tribonacci numbers
+possibilities = [1, 1, 1, 2, 4, 7, 11, 21, 39, 71, 131, 241, 443, 815]
+
+total_possibilities = input.chunk_while do |adapter, next_adapter|
+  next_adapter - adapter == 1
+end.map do |chunk|
+  possibilities[chunk.length]
+end.inject(:*)
+
+puts "Total Possibilities:"
+puts total_possibilities
