12b439d975251dda06853659d222328a31834d5d
Kate Studwell <katestud@github.com>
2020-12-12 10:04:46 -0500

    Add ugly part 2

--- 2020/day_12/rain_risk.cr
+++ 2020/day_12/rain_risk.cr
@@ -14,6 +14,8 @@ class RainRisk
     @input = File.read_lines(input_file)
     @direction = 'E'
     @position = [0, 0]
+    @fourway_position = [0, 0, 0, 0]
+    @waypoint = [1, 10, 0, 0]
   end
 
   def manhattan_distance
@@ -52,6 +54,55 @@ class RainRisk
       @direction = @directions[current_index + steps]
     when 'L'
       @direction = @directions[current_index - steps]
+    end
+  end
+
+  def manhattan_distance_waypoint
+    @input.each do |instruction|
+      behavior = instruction[0]
+      count = instruction[1..-1].to_i
+      if behavior.in_set?("NSEWF")
+        move_waypoint(behavior, count)
+      else
+        rotate_waypoint(behavior, count)
+      end
+    end
+    puts @waypoint
+    (@fourway_position[0] - @fourway_position[2]).abs + (@fourway_position[1] - @fourway_position[3])
+  end
+
+  def move_waypoint(direction, amount)
+    case direction
+    when 'F'
+      @fourway_position[0] += @waypoint[0] * amount
+      @fourway_position[1] += @waypoint[1] * amount
+      @fourway_position[2] += @waypoint[2] * amount
+      @fourway_position[3] += @waypoint[3] * amount
+    when 'N'
+      @waypoint[0] += amount
+    when 'S'
+      @waypoint[2] += amount
+    when 'E'
+      @waypoint[1] += amount
+    when 'W'
+      @waypoint[3] += amount
+    end
+  end
+
+  def rotate_waypoint(direction, amount)
+    steps = (amount / 90).to_i
+    current_index = @directions.index(@direction) || 0
+    case direction
+    when 'R'
+      steps.times do
+        val = @waypoint.pop
+        @waypoint.unshift(val)
+      end
+    when 'L'
+      steps.times do
+        val = @waypoint.shift
+        @waypoint << val
+      end
     end
   end
 
--- 2020/day_12/rain_risk_test.cr
+++ 2020/day_12/rain_risk_test.cr
@@ -2,9 +2,15 @@ require "spec"
 require "./rain_risk"
 
 describe RainRisk do
-	it "finds the manhatttan distance" do
+	it "part 1 finds the manhatttan distance" do
     RainRisk.new("test_input.txt").manhattan_distance.should eq 25
 
     RainRisk.new("input.txt").manhattan_distance.should eq 364
+  end
+
+  it "moves the ship relative to the waypoint" do
+    RainRisk.new("test_input.txt").manhattan_distance_waypoint.should eq 286
+
+    RainRisk.new("input.txt").manhattan_distance_waypoint.should eq 39518
 	end
 end
