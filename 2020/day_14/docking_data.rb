7fd92366b36872d94e1dd1a02f1be17e0d76a1f2
Kate Studwell <katestud@github.com>
2020-12-15 16:25:02 -0500

    Add really janky part 2 for day 14

--- 2020/day_14/docking_data.rb
+++ 2020/day_14/docking_data.rb
@@ -13,9 +13,44 @@ class MemoryHash < Hash
       else
         "0"
       end
-    end.reverse.join
+    end.reverse.join.to_i(2)
 
     super(index, new_val)
+  end
+end
+class MemoryDecoderHash < Hash
+
+  attr_accessor :mask
+
+  # If the bitmask bit is 0, the corresponding memory address bit is unchanged.
+  # If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1.
+  # If the bitmask bit is X, the corresponding memory address bit is floating.
+
+  def []=(position, val)
+    position = position.to_s(2).reverse.each_char.to_a
+    result = mask.reverse.each_char.to_a.map.with_index do |char, index|
+      if char == "0"
+        position[index] || "0"
+      elsif char == "1"
+        "1"
+      else
+        "X"
+      end
+    end.reverse.join
+
+    # Hat tip to @jhawthorn for the flat_map idea from below
+    floats = [result]
+
+    result.count("X").times do
+      floats =
+        floats.flat_map do |x|
+          [x.sub("X", "0"), x.sub("X", "1")]
+        end
+    end
+
+    floats.map{ |x| x.to_i(2) }.each do |float|
+      super(float, val)
+    end
   end
 end
 
@@ -24,9 +59,9 @@ class DockingData
 
   attr_reader :mem
 
-  def initialize(filename)
+  def initialize(filename, part: 1)
     @input = File.readlines(filename, chomp: true)
-    @mem = MemoryHash.new
+    @mem = part == 1 ? MemoryHash.new : MemoryDecoderHash.new
   end
 
   def find_sum
@@ -37,7 +72,7 @@ class DockingData
         eval(line)
       end
     end
-    mem.values.map{ |v| v.to_i(2) }.sum
+    mem.values.sum
   end
 
   def mask=(mask)
@@ -49,4 +84,10 @@ sum = DockingData.new("test_input.txt").find_sum
 puts "TEST INPUT SUM IS: #{sum}"
 
 sum = DockingData.new("input.txt").find_sum
+puts "SUM IS: #{sum}"
+
+sum = DockingData.new("test_input.txt", part: 2).find_sum
+puts "TEST INPUT SUM IS: #{sum}"
+
+sum = DockingData.new("input.txt", part: 2).find_sum
 puts "SUM IS: #{sum}"
