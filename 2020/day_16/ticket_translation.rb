81e2f140536bc239cb8d797675157a62e9ee9c92
Kate Studwell <katestud@github.com>
2020-12-16 14:51:36 -0500

    Part 2 solution

--- 2020/day_16/ticket_translation.rb
+++ 2020/day_16/ticket_translation.rb
@@ -28,6 +28,8 @@ rules.gsub!(":", ": [")
 rules.gsub!("-", "..")
 rules = eval("{#{rules}]}")
 
+your_ticket = your_ticket.split("\n")[1].split(",").map(&:to_i)
+
 nearby_tickets = nearby_tickets.split("\n").drop(1).map do |line|
   line.split(",").map(&:to_i)
 end
@@ -40,3 +42,47 @@ end.sum
 
 puts "SUM FOR PART 1:"
 puts invalid_sum
+
+valid_tickets = nearby_tickets.select do |ticket|
+  ticket.none? { |num| overlapping_ranges.none? { |range| range.cover?(num) } }
+end
+
+valid_tickets << your_ticket
+
+rule_positions = Hash.new { |h, k| h[k] = [] }
+rule_keys = rules.keys
+
+rule_keys.each do |rule_name|
+  ranges = rules[rule_name]
+
+  rule_keys.length.times do |i|
+    position = i
+    valid = valid_tickets.all? do |ticket|
+      ranges.find { |range| range.cover?(ticket[position]) }
+    end
+
+    if valid
+      rule_positions[rule_name] << position
+    end
+  end
+end
+
+possible_positions = (0...rule_keys.length).to_a
+
+inverted = rule_positions.invert
+
+final_positions = {}
+
+inverted.keys.sort_by {|k| k.length }.each do |key|
+  rule_name = inverted[key]
+  correct_position = (possible_positions & key).first
+  final_positions[rule_name] = correct_position
+  possible_positions -= [correct_position]
+end
+
+product = rule_keys.select { |r| r =~ /departure/ }.map do |rule_name|
+  your_ticket[final_positions[rule_name]]
+end.inject(&:*)
+
+puts "PRODUCT FOR PART 2:"
+puts product
