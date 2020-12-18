d46d7afec824210823ddaa69cd9896c0208ad05d
Kate Studwell <katestud@github.com>
2020-12-18 15:52:08 -0500

    Add part 2

--- 2020/day_18/operation_order.rb
+++ 2020/day_18/operation_order.rb
@@ -8,7 +8,20 @@ def calculate_value(value)
   val
 end
 
-input = File.readlines("input.txt", chomp: true).map do |line|
+def calculate_value_precedence(value)
+  while value =~ /\+/
+    value.sub!(/(\d+)([\+])(\d+)/) do |_|
+      match = Regexp.last_match
+      match[1].to_i.send(match[2], match[3].to_i).to_s
+    end
+  end
+  calculate_value(value)
+end
+
+input = File.readlines("input.txt", chomp: true)
+
+## Part 1
+part1 = input.map do |line|
   while line =~ /\)/
     line.gsub!(/\([^()]*?\)/) do |match|
       calculate_value(match[1..-2])
@@ -17,4 +30,19 @@ input = File.readlines("input.txt", chomp: true).map do |line|
   calculate_value(line)
 end
 
-puts input.sum
+puts "Part 1:"
+puts part1.sum
+
+## Part 2
+part2 = File.readlines("input.txt", chomp: true).map do |line|
+  line.gsub!(/\s+/, "")
+  while line =~ /\)/
+    line.gsub!(/\([^()]*?\)/) do |match|
+      calculate_value_precedence(match[1..-2])
+    end
+  end
+  calculate_value_precedence(line)
+end
+
+puts "Part 2:"
+puts part2.sum
