c04c8f6fae60e7ef9086e0bc7f0395328c301746
Kate Studwell <katestud@github.com>
2020-12-21 11:08:16 -0500

    day 21 part 2

--- 2020/day_21/allergen_assessment.rb
+++ 2020/day_21/allergen_assessment.rb
@@ -1,8 +1,6 @@
 INGREDIENT_MATCH = /(?<ingredients>.+)(\(contains\s)(?<allergens>.+)\)/
 
 total_ingredients = []
-
-
 possible_ingredients = {}
 
  File.readlines("input.txt", chomp: true).each do |line|
@@ -25,3 +23,21 @@ all_possible_allergens = possible_ingredients.values.flatten
 non_allergens = total_ingredients - all_possible_allergens
 
 puts non_allergens.count
+
+# Part 2
+count = possible_ingredients.keys.count
+ingredient_by_allergen = {}
+
+until ingredient_by_allergen.keys.count == count
+  ingredient, allergens = possible_ingredients.find { |k, v| v.length == 1 }
+  ingredient_by_allergen[ingredient] = allergens.first
+  possible_ingredients.transform_values! { |v| v - allergens }
+end
+
+puts ingredient_by_allergen
+
+result = ingredient_by_allergen.keys.sort.map do |a|
+  ingredient_by_allergen[a]
+end.join(",")
+
+puts result
