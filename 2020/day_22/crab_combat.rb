2047042e9668e3e59831b6486484c116222f49ec
Kate Studwell <katestud@github.com>
2020-12-22 11:49:11 -0500

    Clean up logic a bit

--- 2020/day_22/crab_combat.rb
+++ 2020/day_22/crab_combat.rb
@@ -16,11 +16,9 @@ def play_game_one(deck_one, deck_two)
     one = deck_one.shift
     two = deck_two.shift
     if one > two
-      deck_one << one
-      deck_one << two
+      deck_one.push one, two
     else
-      deck_two << two
-      deck_two << one
+      deck_two.push two, one
     end
   end
   [deck_one, deck_two]
@@ -29,19 +27,11 @@ end
 def play_game_two(deck_one, deck_two)
   gameset = Set.new([deck_one, deck_two])
   until deck_one.empty? || deck_two.empty?
-    unless gameset.add?([deck_one, deck_two])
-      break
-    end
+    break unless gameset.add?([deck_one, deck_two])
     one = deck_one.shift
     two = deck_two.shift
     winning_deck, winning_card, losing_card = find_winner(one, deck_one, two, deck_two)
-    if winning_deck == deck_one
-      deck_one << winning_card
-      deck_one << losing_card
-    else
-      deck_two << winning_card
-      deck_two << losing_card
-    end
+    winning_deck.push winning_card, losing_card
   end
   [deck_one, deck_two]
 end
@@ -49,13 +39,8 @@ end
 def find_winner(card_one, deck_one, card_two, deck_two)
   if card_one <= deck_one.count && card_two <= deck_two.count
     one, two = play_game_two(deck_one.take(card_one), deck_two.take(card_two))
-    if one.empty?
-      return [deck_two, card_two, card_one]
-    else
-      return [deck_one, card_one, card_two]
-    end
-  end
-  if card_one > card_two
+    one.empty? ? [deck_two, card_two, card_one] :  [deck_one, card_one, card_two]
+  elsif card_one > card_two
     [deck_one, card_one, card_two]
   else
     [deck_two, card_two, card_one]
