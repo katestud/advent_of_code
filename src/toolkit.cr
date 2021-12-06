d6c1c5ee848e3504a4eae314e55e3ab7df59fb96
Kate Studwell <katestud@github.com>
2021-12-06 15:09:33 -0500

    Add initial toolkit for crystal

---
+++ src/toolkit.cr
@@ -0,0 +1,22 @@
+
+# Reads a multi-line file, with each line split by given separator
+def read_lines_with_separator(filename, separator)
+  File.read_lines(filename).map { |line| line.split(separator) }
+end
+
+# Reads a multi-line file with an integer on each line, returns an Array of integers
+def read_lines_of_integers(filename)
+  File.read_lines(filename).map { |line| line.to_i }
+end
+
+# Reads a one-line file with comma-separated integers, returns an Array of integers
+def read_line_of_integers(filename)
+  File.read(filename).split(",").map { |line| line.to_i }
+end
+
+# Generates a grid (array of arrays) of a given size, with the given placeholder
+def grid(size, placeholder)
+  grid = [Array.new(size, placeholder)]
+  (size-1).times { grid <<  Array.new(size, placeholder) }
+  grid
+end
