da96158b607a5f9dff054878408809546431915e
Kate Studwell <katestud@github.com>
2021-12-06 04:48:56 -0500

    Slightly cleaned up

--- 2021/day_06/lanternfish.cr
+++ 2021/day_06/lanternfish.cr
@@ -1,7 +1,12 @@
 class Lanternfish
   @fish : Array(Int32)
+  @fish_counts : Hash(Int32, Int128)
   def initialize(input_file = "input.txt")
     @fish = File.read_lines(input_file).first.split(",").map {|i| i.to_i}
+    @fish_counts = Hash(Int32, Int128).new(0)
+    (0..8).each do |i|
+      @fish_counts[i] = @fish.count(i).to_i128
+    end
   end
 
   def execute_one
@@ -12,24 +17,16 @@ class Lanternfish
     execute(256)
   end
 
-
   def execute(times)
-    counts = Hash(Int32, Int128).new(0)
-    (0..8).each do |i|
-      counts[i] = @fish.count(i).to_i128
-    end
     times.times do
-      zeros = counts[0]
       new_counts = Hash(Int32, Int128).new(0)
       (1..8).to_a.reverse.each do |i|
-        j = i-1
-        new_counts[j] = counts[i]
+        new_counts[i-1] = @fish_counts[i]
       end
-      new_counts[8] = zeros
+      new_counts[8] = @fish_counts[0]
       new_counts[6] = new_counts[6] + new_counts[8]
-      counts = new_counts
-      # puts counts.inspect
+      @fish_counts = new_counts
     end
-    counts.values.sum
+    @fish_counts.values.sum
   end
 end
