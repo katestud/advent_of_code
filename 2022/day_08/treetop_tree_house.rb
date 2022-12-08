3a2847e238566d6959dd6ede0f78cb053aaf7b2c
Kate Studwell <katestud@github.com>
2022-12-08 08:48:09 -0500

    Solve for part 2: grids all the way down 🔲🔳

--- 2022/day_08/treetop_tree_house.rb
+++ 2022/day_08/treetop_tree_house.rb
@@ -3,6 +3,7 @@ class TreetopTreeHouse
   def initialize(file_name = "input.txt")
     @file_name = file_name
     @grid = read_file.map { |l| l.chars.map(&:to_i) }
+    @highest_scenic_score = 0
   end
 
   def execute_one
@@ -16,13 +17,54 @@ class TreetopTreeHouse
   end
 
   def execute_two
-    read_file.length
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |cell, cell_index|
+        next if perimeter?(row_index, cell_index)
+        score = score_for_cell(row_index, cell_index)
+        @highest_scenic_score = score if score > @highest_scenic_score
+      end
+    end
+    @highest_scenic_score
   end
 
   private
 
   def read_file
     File.readlines(@file_name, chomp: true)
+  end
+
+  def score_for_cell(row_index, cell_index)
+    cell_value = @grid[row_index][cell_index]
+    score_right = score_to_the_right(row_index, cell_index, cell_value)
+    score_left = score_to_the_left(row_index, cell_index, cell_value)
+    score_above = score_above(row_index, cell_index, cell_value)
+    score_below = score_below(row_index, cell_index, cell_value)
+    score_right * score_left * score_above * score_below
+  end
+
+  def score_to_the_right(row_index, cell_index, cell_value)
+    count_score @grid[row_index][cell_index+1..-1], cell_value
+  end
+
+  def score_to_the_left(row_index, cell_index, cell_value)
+    count_score @grid[row_index][0..cell_index-1].reverse, cell_value
+  end
+
+  def score_above(row_index, cell_index, cell_value)
+    count_score @grid[0..row_index-1].map { |r| r[cell_index] }.reverse, cell_value
+  end
+
+  def score_below(row_index, cell_index, cell_value)
+    count_score @grid[row_index+1..-1].map { |r| r[cell_index] }, cell_value
+  end
+
+  def count_score(arr, cell_value)
+    count = 0
+    arr.each do |cell|
+      count += 1
+      break if cell >= cell_value
+    end
+    count
   end
 
   def visible?(row_index, cell_index)
--- 2022/day_08/treetop_tree_house_test.rb
+++ 2022/day_08/treetop_tree_house_test.rb
@@ -6,12 +6,12 @@ class TreetopTreeHouseTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 21, TreetopTreeHouse.new("test_input.txt").execute_one
 
-    assert_equal 0, TreetopTreeHouse.new("input.txt").execute_one
+    assert_equal 1693, TreetopTreeHouse.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    # assert_equal 0, TreetopTreeHouse.new("test_input.txt").execute_two
+    assert_equal 8, TreetopTreeHouse.new("test_input.txt").execute_two
 
-    # assert_equal 0, TreetopTreeHouse.new("input.txt").execute_two
+    assert_equal 422059, TreetopTreeHouse.new("input.txt").execute_two
   end
 end
