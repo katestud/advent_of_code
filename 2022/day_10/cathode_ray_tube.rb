c3d5f068175974fbff11b0a53086c1f8d6240567
Kate Studwell <katestud@github.com>
2022-12-10 05:03:15 -0500

    Day 10 part 2

--- 2022/day_10/cathode_ray_tube.rb
+++ 2022/day_10/cathode_ray_tube.rb
@@ -3,13 +3,51 @@ class CathodeRayTube
   def initialize(file_name = "input.txt")
     @file_name = file_name
     @register = 1
-    @registers = [0, 1]
-    @cycles = [20, 60, 100, 140, 180, 220]
+    @registers = [@register]
   end
 
   # Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
 
   def execute_one
+    build_registers
+    [20, 60, 100, 140, 180, 220].map do |cycle|
+      @registers[cycle - 1] * cycle
+    end.sum
+  end
+
+  # Start cycle   1: begin executing addx 15
+  # During cycle  1: CRT draws pixel in position 0
+  # Current CRT row: #
+
+  # During cycle  2: CRT draws pixel in position 1
+  # Current CRT row: ##
+  # End of cycle  2: finish executing addx 15 (Register X is now 16)
+  # Sprite position: ...............###......................
+
+  # Start cycle   3: begin executing addx -11
+  # During cycle  3: CRT draws pixel in position 2
+  # Current CRT row: ##.
+
+  def execute_two
+    build_registers
+    @registers.map.with_index do |register, index|
+      position = index % 40
+      if (position-1..position+1).cover?(register)
+        "#"
+      else
+        "."
+      end
+    end.each_slice(40) { |row| puts row.join }
+    nil
+  end
+
+  private
+
+  def read_file
+    File.readlines(@file_name, chomp: true)
+  end
+
+  def build_registers
     read_file.each do |instruction|
       behavior, amount = instruction.split
       @registers << @register
@@ -21,19 +59,6 @@ class CathodeRayTube
         #noop
       end
     end
-    @cycles.map do |cycle|
-      @registers[cycle] * cycle
-    end.sum
-  end
-
-  def execute_two
-    read_file.length
-  end
-
-  private
-
-  def read_file
-    File.readlines(@file_name, chomp: true)
   end
 
 end
--- 2022/day_10/cathode_ray_tube_test.rb
+++ 2022/day_10/cathode_ray_tube_test.rb
@@ -6,12 +6,12 @@ class CathodeRayTubeTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 13140, CathodeRayTube.new("test_input.txt").execute_one
 
-    assert_equal 0, CathodeRayTube.new("input.txt").execute_one
+    assert_equal 14240, CathodeRayTube.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    assert_equal 0, CathodeRayTube.new("test_input.txt").execute_two
+    assert_nil CathodeRayTube.new("test_input.txt").execute_two
 
-    assert_equal 0, CathodeRayTube.new("input.txt").execute_two
+    assert_nil CathodeRayTube.new("input.txt").execute_two
   end
 end
