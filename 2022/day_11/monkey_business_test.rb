d0cbf79afac4e9cffef88c65dfb10837cb6ab910
Kate Studwell <katestud@github.com>
2022-12-11 10:29:16 -0500

    Fix test assertions

--- 2022/day_11/monkey_business_test.rb
+++ 2022/day_11/monkey_business_test.rb
@@ -6,12 +6,12 @@ class MonkeyBusinessTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 10605, MonkeyBusiness.new("test_input.txt").execute_one
 
-    assert_equal 2713310158, MonkeyBusiness.new("input.txt").execute_one
+    assert_equal 56595, MonkeyBusiness.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    # assert_equal 0, MonkeyBusiness.new("test_input.txt").execute_two
+    assert_equal 2713310158, MonkeyBusiness.new("test_input.txt").execute_two
 
-    # assert_equal 0, MonkeyBusiness.new("input.txt").execute_two
+    assert_equal 0, MonkeyBusiness.new("input.txt").execute_two
   end
 end
