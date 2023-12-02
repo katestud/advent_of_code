e7140e1b78763f3f7f899f61ea9a66d2788a2456
Kate Studwell <katestudwell@gmail.com>
2023-12-02 00:28:22 -0500

    Day 2 part 2 solution, initial attempt

--- 2023/day_2/cube_conundrum.rb
+++ 2023/day_2/cube_conundrum.rb
@@ -2,25 +2,28 @@ require_relative "../../src/toolkit"
 
 class CubeConundrum
 
-  # The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-
+  # Example input line:
   # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 
   def initialize(file_name = "input.txt")
     @input = File.readlines(file_name, chomp: true)
   end
 
+  # The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
   def execute_one
     @input.map do |game|
-      split_game(game)
+      valid_game_id(game)
     end.sum
   end
 
+  # The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
   def execute_two
-    @input.length
+    map = @input.map do |game|
+      game_power(game)
+    end.sum
   end
 
-  def split_game(line)
+  def valid_game_id(line)
     identifier, instructions = line.split(": ")
     id = identifier.split(" ").last.to_i
 
@@ -33,5 +36,15 @@ class CubeConundrum
     return 0 if blue.any? { |match| match.to_i > 14 }
 
     id
+  end
+
+  def game_power(line)
+    _, instructions = line.split(": ")
+
+    [
+      instructions.scan(/\d+\sred/).map(&:to_i).max,
+      instructions.scan(/\d+\sgreen/).map(&:to_i).max,
+      instructions.scan(/\d+\sblue/).map(&:to_i).max
+    ].compact.reduce(:*)
   end
 end
--- 2023/day_2/cube_conundrum_test.rb
+++ 2023/day_2/cube_conundrum_test.rb
@@ -6,12 +6,12 @@ class CubeConundrumTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 8, CubeConundrum.new("test_input.txt").execute_one
 
-    assert_equal 0, CubeConundrum.new("input.txt").execute_one
+    assert_equal 2551, CubeConundrum.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    assert_equal 0, CubeConundrum.new("test_input.txt").execute_two
+    assert_equal 2286, CubeConundrum.new("test_input.txt").execute_two
 
-    # assert_equal 0, CubeConundrum.new("input.txt").execute_two
+    assert_equal 62811, CubeConundrum.new("input.txt").execute_two
   end
 end
