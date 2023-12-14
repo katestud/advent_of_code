05124ed2d58e91b4acf6fd98fb1525fd6456cdfe
Kate Studwell <katestudwell@gmail.com>
2023-12-14 09:02:48 -0500

    Part 2

--- 2023/day_14/parabolic_reflector.rb
+++ 2023/day_14/parabolic_reflector.rb
@@ -7,25 +7,85 @@ class ParabolicReflector
   end
 
   def execute_one
-    transposed = @input.transpose
-    rolled = transposed.map do |column|
+    score(tilt_north(@input))
+  end
+
+  def execute_two
+    platform = cycle(@input, 1_000_000_000)
+    score(platform)
+  end
+
+  private
+
+  def cycle(platform, count)
+    cycle_configs = {}
+    cache_hit_found = false
+
+    i = 0
+    until i >= count
+      platform = full_tilt(platform)
+
+      key = platform.join
+      prev_cycle = cycle_configs[key]
+      if prev_cycle && !cache_hit_found
+        num_cycles_since = i - prev_cycle
+        cycle_configs[key] = i
+        i = i + (((count - i) / num_cycles_since) * num_cycles_since) + 1
+        cache_hit_found = true
+      else
+        cycle_configs[key] = i
+        i += 1
+      end
+    end
+    platform
+  end
+
+  def full_tilt(platform)
+    platform = tilt_north(platform)
+    platform = tilt_west(platform)
+    platform = tilt_south(platform)
+    platform = tilt_east(platform)
+  end
+
+  def tilt_north(platform)
+    tilt(platform.transpose).transpose
+  end
+
+  def tilt_west(platform)
+    tilt(platform)
+  end
+
+  def tilt_south(platform)
+    tilt(platform.transpose, true).transpose
+  end
+
+  def tilt_east(platform)
+    tilt(platform, true)
+  end
+
+  def tilt(platform, down = false)
+    platform.map do |column|
       length = column.size
       chunks = column.join.split("#")
       new_string = chunks.map do |chunk|
-         chunk.each_char.sort.reverse.join
+        sorted = chunk.each_char.sort
+        if !down
+          sorted.reverse!
+        end
+        sorted.join
       end.join("#")
       if length > new_string.size
         new_string += "#" * (length-new_string.size)
       end
       new_string.each_char.to_a
     end
-    length = rolled.length
-    rolled.transpose.map.with_index do |row, index|
+  end
+
+  def score(platform)
+    length = platform.length
+    platform.map.with_index do |row, index|
       val = row.count { |r| r == "O"} * (length - index)
     end.sum
   end
 
-  def execute_two
-    @input.length
-  end
 end
--- 2023/day_14/parabolic_reflector_test.rb
+++ 2023/day_14/parabolic_reflector_test.rb
@@ -6,12 +6,12 @@ class ParabolicReflectorTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 136, ParabolicReflector.new("test_input.txt").execute_one
 
-    assert_equal 0, ParabolicReflector.new("input.txt").execute_one
+    assert_equal 110407, ParabolicReflector.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    assert_equal 0, ParabolicReflector.new("test_input.txt").execute_two
+    assert_equal 64, ParabolicReflector.new("test_input.txt").execute_two
 
-    # assert_equal 0, ParabolicReflector.new("input.txt").execute_two
+    assert_equal 87273, ParabolicReflector.new("input.txt").execute_two
   end
 end
