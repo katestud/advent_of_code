9c46832b9a5c6f4f7bb671a9f3f9e37a2b4ce369
Kate Studwell <katestudwell@gmail.com>
2024-12-02 09:35:21 -0500

    Ugly part 2

--- 2024/day_01/historian_hysteria.rb
+++ 2024/day_01/historian_hysteria.rb
@@ -3,24 +3,27 @@ require_relative "../../src/toolkit"
 class HistorianHysteria
 
   def initialize(file_name = "input.txt")
-    @input = File.readlines(file_name, chomp: true)
-
+    @right = []
+    @left = []
+    File.readlines(file_name, chomp: true).each do |line|
+      l, r = line.split(" ")
+      @right << r.to_i
+      @left << l.to_i
+    end
   end
 
   def execute_one
-    right = []
-    left = []
-    @input.each do |line|
-      l, r = line.split(" ")
-      right << r.to_i
-      left << l.to_i
-    end
-    left.sort.zip(right.sort).map do |l,r|
+    @left.sort.zip(@right.sort).map do |l,r|
       (l - r).abs
     end.sum
   end
 
   def execute_two
-    @input.length
+    right_counts = @right.each_with_object(Hash.new(0)) do |location, hash|
+      hash[location] += 1
+    end
+    @left.map do |l|
+      right_counts[l] * l
+    end.sum
   end
 end
--- 2024/day_01/historian_hysteria_test.rb
+++ 2024/day_01/historian_hysteria_test.rb
@@ -10,8 +10,8 @@ class HistorianHysteriaTest < Minitest::Test
   end
 
   def test_that_part_two_works
-    assert_equal 0, HistorianHysteria.new("test_input.txt").execute_two
+    assert_equal 31, HistorianHysteria.new("test_input.txt").execute_two
 
-    # assert_equal 0, HistorianHysteria.new("input.txt").execute_two
+    assert_equal 0, HistorianHysteria.new("input.txt").execute_two
   end
 end
