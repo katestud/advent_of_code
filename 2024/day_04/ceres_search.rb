036017ba400dead76166c5d0286b58e99c0db6a3
Kate Studwell <katestudwell@gmail.com>
2024-12-04 02:09:31 -0500

    Overly verbose part 2

--- 2024/day_04/ceres_search.rb
+++ 2024/day_04/ceres_search.rb
@@ -28,7 +28,33 @@ class CeresSearch
   end
 
   def execute_two
-    @grid.length
+    found = 0
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |column, column_index|
+        next unless column == "A"
+        pos = [row_index, column_index]
+        right_diag = [[-1, -1],[1,1]]
+        left_diag = [[-1, 1], [1, -1]]
+        vals = right_diag.map do |right_pos|
+          next_step = valid_next_step(pos, right_pos, @grid_size)
+          if next_step
+            @grid[next_step[0]][next_step[1]]
+          end
+        end
+        next if vals.any? {|v| v.nil? }
+        next unless vals.sort == ["M", "S"]
+        vals = left_diag.map do |left_pos|
+          next_step = valid_next_step(pos, left_pos, @grid_size)
+          if next_step
+            @grid[next_step[0]][next_step[1]]
+          end
+        end
+        next if vals.any? {|v| v.nil? }
+        next unless vals.sort == ["M", "S"]
+        found += 1
+      end
+    end
+    found
   end
 
   private
--- 2024/day_04/ceres_search_test.rb
+++ 2024/day_04/ceres_search_test.rb
@@ -6,12 +6,12 @@ class CeresSearchTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 18, CeresSearch.new("test_input.txt").execute_one
 
-    assert_equal 0, CeresSearch.new("input.txt").execute_one
+    puts "Part 1: #{CeresSearch.new("input.txt").execute_one}"
   end
 
   def test_that_part_two_works
-    assert_equal 0, CeresSearch.new("test_input.txt").execute_two
+    assert_equal 9, CeresSearch.new("test_input.txt").execute_two
 
-    # assert_equal 0, CeresSearch.new("input.txt").execute_two
+    puts "Part 2: #{CeresSearch.new("input.txt").execute_two}"
   end
 end
