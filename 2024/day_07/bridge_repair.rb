a9cf5b58cc6a89271f3aeab7fb50edda731a169b
Kate Studwell <katestudwell@gmail.com>
2024-12-07 02:14:55 -0500

    Complex part 2

--- 2024/day_07/bridge_repair.rb
+++ 2024/day_07/bridge_repair.rb
@@ -7,15 +7,38 @@ class BridgeRepair
   end
 
   def execute_one
+    perform(['*', '+'])
+  end
+
+  def execute_two
+    perform(['*', '+', '||'])
+  end
+
+  private
+
+  def evaluate_instructions(instructions)
+    left, operator, right = instructions
+    case operator
+    when "*"
+      left.to_i * right.to_i
+    when "+"
+      left.to_i + right.to_i
+    when "||"
+      "#{left}#{right}".to_i
+    end
+  end
+
+  def perform(operator_options)
     sum_found_values = 0
     @input.each do |line|
       first, *values = line.split
+      values.map!(&:to_i)
       expected = first.to_i
-      ['+', '*'].repeated_permutation(values.length - 1).each do |perm|
+      operator_options.repeated_permutation(values.length - 1).each do |perm|
         acc, *rest = values
         perm.each_with_index do |operator, index|
-          str = "#{acc} #{operator} #{rest[index]}"
-          acc = eval(str)
+          break if acc > expected
+          acc = evaluate_instructions [acc, operator, rest[index]]
         end
         if acc == expected
           sum_found_values += expected
@@ -26,7 +49,4 @@ class BridgeRepair
     sum_found_values
   end
 
-  def execute_two
-    @input.length
-  end
 end
