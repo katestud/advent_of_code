f737ac09a4bef65c8da11c7a14a944cd3ee52da9
Kate Studwell <katestudwell@gmail.com>
2024-12-08 05:13:42 -0500

    ugly part 2

--- 2024/day_08/resonant_collinearity.rb
+++ 2024/day_08/resonant_collinearity.rb
@@ -20,32 +20,61 @@ class ResonantCollinearity
   end
 
   def execute_one
-    @antinodes = Set.new
+    antinodes = Set.new
     @antenna_positions.each do |_, positions|
       positions.each do |pos|
         positions.each do |next_pos|
-          next if pos == next_pos
-          x, y = pos
-          nx, ny = next_pos
-          x_diff = x - nx
-          y_diff = y - ny
-          node_1 = [x + x_diff, y + y_diff]
-          @antinodes << node_1 if within_bounds?(node_1)
-          node_2 = [nx - x_diff, ny - y_diff]
-          @antinodes << node_2 if within_bounds?(node_2)
+          find_antinodes(pos, next_pos).each { |n| antinodes.add(n) }
         end
       end
     end
-    @antinodes.size
+    antinodes.size
   end
 
   def execute_two
-    @antenna_positions.length
+    antinodes = Set.new
+    @antenna_positions.each do |_, positions|
+      positions.each do |pos|
+        antinodes.add(pos)
+        positions.each do |next_pos|
+          find_antinodes(pos, next_pos, true).each { |n| antinodes.add(n) }
+        end
+      end
+    end
+    antinodes.size
   end
 
   private
 
   def within_bounds?(pos)
     pos.all? { |i| i >= 0 && i < @grid_size }
+  end
+
+  def find_antinodes(pos1, pos2, resonant = false)
+    return [] if pos1 == pos2
+    x, y = pos1
+    nx, ny = pos2
+    x_diff = x - nx
+    y_diff = y - ny
+    if resonant
+      nodes = []
+      dir_1 = [x + x_diff, y + y_diff]
+      while within_bounds?(dir_1)
+        nodes << dir_1
+        x, y = dir_1
+        dir_1 = [x + x_diff, y + y_diff]
+      end
+      dir_2 = [nx - x_diff, ny - y_diff]
+      while within_bounds?(dir_2)
+        nodes << dir_2
+        x, y = dir_2
+        dir_2 = [x - x_diff, y - y_diff]
+      end
+      nodes
+    else
+      node_1 = [x + x_diff, y + y_diff]
+      node_2 = [nx - x_diff, ny - y_diff]
+      [node_1, node_2].select { |n| within_bounds?(n) }
+    end
   end
 end
--- 2024/day_08/resonant_collinearity_test.rb
+++ 2024/day_08/resonant_collinearity_test.rb
@@ -6,12 +6,12 @@ class ResonantCollinearityTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 14, ResonantCollinearity.new("test_input.txt").execute_one
 
-    assert_equal 0, ResonantCollinearity.new("input.txt").execute_one
+    assert_equal 249, ResonantCollinearity.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    assert_equal 0, ResonantCollinearity.new("test_input.txt").execute_two
+    assert_equal 34, ResonantCollinearity.new("test_input.txt").execute_two
 
-    # assert_equal 0, ResonantCollinearity.new("input.txt").execute_two
+    assert_equal 0, ResonantCollinearity.new("input.txt").execute_two
   end
 end
