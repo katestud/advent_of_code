f70d944f1d873adca00262c9690ac0af610a55ac
Kate Studwell <katestudwell@gmail.com>
2025-12-02 14:13:10 -0500

    Smol refactor in prep for part 2

--- 2025/day_02/gift_shop.rb
+++ 2025/day_02/gift_shop.rb
@@ -2,38 +2,64 @@ require_relative "../../src/toolkit"
 
 class GiftShop
 
-# Since the young Elf was just doing silly patterns, you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
-#
-#11-22 has two invalid IDs, 11 and 22.
-# 95-115 has one invalid ID, 99.
-# 998-1012 has one invalid ID, 1010.
-# 1188511880-1188511890 has one invalid ID, 1188511885.
-# 222220-222224 has one invalid ID, 222222.
-# 1698522-1698528 contains no invalid IDs.
-# 446443-446449 has one invalid ID, 446446.
-# 38593856-38593862 has one invalid ID, 38593859.
-# The rest of the ranges contain no invalid IDs.
-
   def initialize(file_name = "input.txt")
     @input = File.read(file_name, chomp: true).split(",")
+    @ids = @input
+      .map { |line| line.split("-").map(&:to_i) }
+      .map { |b, e| (b..e).to_a }
+      .flatten
   end
 
   def execute_one
     sum = 0
-    @input.each do |range|
-      b, e = range.split("-").map(&:to_i)
-      (b..e).to_a.each do |val|
-        str_val = val.to_s
-        length = str_val.length
-        next unless  length % 2 == 0
-        next unless str_val[0, length / 2] == str_val[length / 2, length]
-        sum += val
-      end
+    @ids.each do |val|
+      sum += val if is_invalid?(val)
     end
     sum
   end
 
   def execute_two
-    @input.length
+    sum = 0
+    @ids.each do |val|
+      sum += val if is_invalid_two?(val)
+    end
+    sum
+  end
+
+  #11-22 has two invalid IDs, 11 and 22.
+  # 95-115 has one invalid ID, 99.
+  # 998-1012 has one invalid ID, 1010.
+  # 1188511880-1188511890 has one invalid ID, 1188511885.
+  # 222220-222224 has one invalid ID, 222222.
+  # 1698522-1698528 contains no invalid IDs.
+  # 446443-446449 has one invalid ID, 446446.
+  # 38593856-38593862 has one invalid ID, 38593859.
+  # The rest of the ranges contain no invalid IDs.
+
+  def is_invalid?(id)
+    str_id = id.to_s
+    length = str_id.length
+    return false unless  length % 2 == 0
+    return false unless str_id[0, length / 2] == str_id[length / 2, length]
+    true
+  end
+
+  # 11-22 still has two invalid IDs, 11 and 22.
+  # 95-115 now has two invalid IDs, 99 and 111.
+  # 998-1012 now has two invalid IDs, 999 and 1010.
+  # 1188511880-1188511890 still has one invalid ID, 1188511885.
+  # 222220-222224 still has one invalid ID, 222222.
+  # 1698522-1698528 still contains no invalid IDs.
+  # 446443-446449 still has one invalid ID, 446446.
+  # 38593856-38593862 still has one invalid ID, 38593859.
+  # 565653-565659 now has one invalid ID, 565656.
+  # 824824821-824824827 now has one invalid ID, 824824824.
+  # 2121212118-2121212124 now has one invalid ID, 2121212121.
+  def is_invalid_two?(id)
+    str_id = id.to_s
+    length = str_id.length
+    return false unless  length % 2 == 0
+    return false unless str_id[0, length / 2] == str_id[length / 2, length]
+    true
   end
 end
--- 2025/day_02/gift_shop_test.rb
+++ 2025/day_02/gift_shop_test.rb
@@ -6,11 +6,11 @@ class GiftShopTest < Minitest::Test
   def test_that_part_one_works
     assert_equal 1227775554, GiftShop.new("test_input.txt").execute_one
 
-    assert_equal 0, GiftShop.new("input.txt").execute_one
+    assert_equal 37314786486, GiftShop.new("input.txt").execute_one
   end
 
   def test_that_part_two_works
-    assert_equal 0, GiftShop.new("test_input.txt").execute_two
+    assert_equal 4174379265, GiftShop.new("test_input.txt").execute_two
 
     # assert_equal 0, GiftShop.new("input.txt").execute_two
   end
