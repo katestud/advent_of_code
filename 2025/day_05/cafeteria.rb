17d1edc165d9867f4f4f738403c0400e4e9539cc
Kate Studwell <katestudwell@gmail.com>
2025-12-06 14:09:39 -0500

    Day 5 part 2

--- 2025/day_05/cafeteria.rb
+++ 2025/day_05/cafeteria.rb
@@ -13,11 +13,28 @@ class Cafeteria
 
   def execute_one
     @available_ids.count do |i|
-      @ranges.any? { |range| range.member?(i) }
+      @ranges.any? { |range| range.cover?(i) }
     end
   end
 
   def execute_two
-    0
+    collapsed = @ranges.sort_by(&:begin)
+    searching = true
+    while searching
+      searching = false
+      ranges = collapsed.dup
+      collapsed = []
+      while ranges.length > 0
+        candidate = ranges.shift
+        friend = ranges.detect { |r| r.cover?(candidate.begin) || r.cover?(candidate.end) || candidate.cover?(r.begin) || candidate.cover?(r.end) }
+        if friend
+          searching = true
+          ranges.delete(friend)
+        end
+        all = [friend, candidate].compact
+        collapsed << (all.map(&:begin).min..all.map(&:end).max)
+      end
+    end
+    collapsed.sum { |r| r.size }
   end
 end
--- 2025/day_05/cafeteria_test.rb
+++ 2025/day_05/cafeteria_test.rb
@@ -10,8 +10,8 @@ class CafeteriaTest < Minitest::Test
   end
 
   def test_that_part_two_works
-    assert_equal 0, Cafeteria.new("test_input.txt").execute_two
+    assert_equal 14, Cafeteria.new("test_input.txt").execute_two
 
-    # assert_equal 0, Cafeteria.new("input.txt").execute_two
+    assert_equal 333892124923577, Cafeteria.new("input.txt").execute_two
   end
 end
